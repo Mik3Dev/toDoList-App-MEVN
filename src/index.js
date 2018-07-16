31a34f0c9609604a82af5b2ad693c1a57ecddd58
mik3dev <miguel.acosta1978@gmail.com>
2018-07-16 14:41:05 -0400

    Adding scss compiler to webpack setup

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import App from './components/App.vue';
+import './scss/main.scss';
 
 new Vue({
     el:'#app',
--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
+    // entry: ['./src/index.js', './src/scss/main.scss '],
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
-        path: path.join(__dirname, 'server/public')
+        path: path.join(__dirname, 'server/public/js')
     },
     module: {
         rules: [{
@@ -23,13 +25,24 @@ module.exports = {
                     presets: ['babel-preset-env']
                 }
             }
+        }, {
+            test: /\.(sass|scss)$/,
+            use: [
+                MiniCssExtractPlugin.loader,
+                'css-loader',
+                'sass-loader'
+            ]
         }]
     },
     plugins: [
         new HtmlWebpackPlugin({
             title: 'ToDo App',
-            template: './src/index.html'
+            template: './src/index.html',
+            filename: './../index.html'
         }),
-        new VueLoaderPlugin()
+        new VueLoaderPlugin(),
+        new MiniCssExtractPlugin({
+            filename: './../css/bundle.css'
+        })
     ]
 }
